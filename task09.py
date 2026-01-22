fc8f9742c8e5edb3ac5c1b797e2d7667f14614ef
Ozodbek Tolliyev <ozodbektolliyev179@gmail.com>
2026-01-22 16:50:21 +0500

    Update task09.py

---
+++ task09.py
@@ -0,0 +1,19 @@
+class User:
+    def __init__(self, username: str, email: str, is_active: bool):
+        self.username = username
+        self.email = email
+        self.is_active = is_active
+
+    def activate(self) -> None:
+        self.is_active = True
+        print(f"{self.username} foydalanuvchisi faollashtirildi ✅")
+
+    def deactivate(self) -> None:
+        self.is_active = False
+        print(f"{self.username} foydalanuvchisi bloklandi ❌")
+
+user1 = User("ozodbek", "ozodbek@gmail.com", False)
+
+
+user1.activate()
+user1.deactivate()
